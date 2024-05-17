f7dea184de06a0b254f6d753c894222961c93222
Devang101Bhatt <DBofficial0001@gmail.com>
2024-05-17 20:24:33 +0530

    safe commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
-#include<iostream>
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
 using namespace std;
 
-int main(){
-    cout<<"initial commit";
-    return 0;
+const int N = 9;
+
+bool isSafe(int board[N][N], int row, int col, int num) {
+    // Check if 'num' is already in the same row
+    for (int i = 0; i < N; i++)
+        if (board[row][i] == num)
+            return false;
+
+    // Check if 'num' is already in the same column
+    for (int i = 0; i < N; i++)
+        if (board[i][col] == num)
+            return false;
+
+    // Check if 'num' is already in the same 3x3 box
+    int boxRowStart = row - row % 3;
+    int boxColStart = col - col % 3;
+
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            if (board[i + boxRowStart][j + boxColStart] == num)
+                return false;
+
+    return true;
 }
