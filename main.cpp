d98d6a2d0c14bed06535043894e4ea029193705c
Devang101Bhatt <DBofficial0001@gmail.com>
2024-05-17 21:28:25 +0530

    main commit

--- main.cpp
+++ main.cpp
@@ -140,3 +140,72 @@ void playGame(int board[N][N]){
     }
 
 }
+
+int main() {
+
+    system("title Sudoku Game @copyassignment");
+    system("color B0");
+    int board[N][N] = {
+        {3, 0, 6, 5, 0, 8, 4, 0, 0},
+        {5, 2, 0, 0, 0, 0, 0, 0, 0},
+        {0, 8, 7, 0, 0, 0, 0, 3, 1},
+        {0, 0, 3, 0, 1, 0, 0, 8, 0},
+        {9, 0, 0, 8, 6, 3, 0, 0, 5},
+        {0, 5, 0, 0, 9, 0, 6, 0, 0},
+        {1, 3, 0, 0, 0, 0, 2, 5, 0},
+        {0, 0, 0, 0, 0, 0, 0, 7, 4},
+        {0, 0, 5, 2, 0, 6, 3, 0, 0}
+    };
+
+    cout << "\t\t\t<================================================================================>" << endl;
+    cout << "\t\t\t|                        WELCOME TO SUDOKU Game!                                 |" << endl;
+    cout << "\t\t\t|       Fill in the missing numbers(represented by 0) to solve the puzzle.       |" << endl;
+    cout << "\t\t\t<================================================================================>" << endl;
+
+    while (true) {
+
+        int choice;
+        cout << endl << endl;
+        cout << "\t\t[1] Solve the Sudoku" << endl;
+        cout << "\t\t[2] Unable to solve? View the solved Sudoku" << endl;
+        cout << "\t\t[3] Exit" << endl;
+        cout << "\t\tEnter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                playGame(board);
+                break;
+            case 2:
+                if (solveSudoku(board, 0, 0))
+                {
+                    cout << "Completely Solved Sudoku is: "<< endl;
+                    cout << endl << endl;
+                    for (int row = 0; row < N; row++){
+                        for (int col = 0; col < N; col++){
+                            if(col == 3 || col == 6)
+                                cout << " | ";
+                            cout << board[row][col] <<" ";
+                        }
+                        if(row == 2 || row == 5){
+                            cout << endl;
+                            for(int i = 0; i<N; i++)
+                                cout << "---";
+                        }
+                        cout << endl;
+                    }
+                    cout << endl;
+                    cout << "Better luck next time!!!" << endl;
+                }
+                else
+                    cout << "No solution found" << endl;
+                break;
+            case 3:
+                exit(0);
+            default:
+                cout << "Invalid choice" << endl;
+        }
+   return 0;
+}
+
+}
